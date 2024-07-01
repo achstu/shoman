d6938109439f8bb787d30d17cf4ec220ba4d4a35
Michał Stachurski <root.stachu@gmail.com>
2024-07-01 12:38:22 +0200

    minimax added

--- src/search.cpp
+++ src/search.cpp
@@ -9,18 +9,34 @@ float minimax(const State& state, int depth = 2) {
   }
 
   // TODO: std::numeric_limits<float>::inifinity();
-  float value = -INFINITY;
+  float score = -INFINITY;
   for (Move move : state.all_moves()) {
     State child = state;
     child.make(move);
 
-    value = std::max(
-      value,
+    score = std::max(
+      score,
       -minimax(child, depth - 1)
     );
   }
-  return value;
+  return score;
 }
 
 // simple alpha-beta approach
-float 
+// alpha := minimum score that the maximizing player is assured of
+// beta := maximum score that the minimizing player is assured of
+float alpha_beta(float alpha, float beta, const State& state, int depth = 2) {
+  if (depth == 0) {
+    return state.evaluate();
+  }
+
+  float 
+  for (Move move : state.all_moves()) {
+    State child = state;
+    child.make(move);
+
+    
+  }
+
+  
+} 
